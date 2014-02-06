497798e980f106c2e29a658d170011b1e27fe968
Wes Hayutin <weshayutin@gmail.com>
2014-02-06 15:53:24 -0500

    minor updates to run.sh for tempest refactor
    Change-Id: I13c1342a66732c6029b6b0fab8e1d729c1bc220a

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ main() {
 source settings.sh
 
 ansible-playbook -i local_hosts  \
- playbooks/packstack/rdo_neutron_aio_playbook.yml \
+ playbooks/packstack/rdo_neutron_aio.yml \
     --extra-vars @settings.yml -v -u $remote_user -s $tags
 }
 
