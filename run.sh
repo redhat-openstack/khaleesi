ce1aedfee6f9117d6a6062f1a9b2bb19284d5470
Wes Hayutin <weshayutin@gmail.com>
2014-02-10 20:47:37 -0500

    run.sh: add checks and clean exit
    add check for repo_settings.yml and a clean exit for clean.sh to
    execute after run.sh
    
    Change-Id: I386db477bbda2d0b537df23102c2583b4166d434

--- run.sh
+++ run.sh
@@ -4,10 +4,21 @@ main() {
 
 source settings.sh
 
-ansible-playbook -i local_hosts  \
- playbooks/packstack/rdo_neutron_aio.yml \
-    --extra-vars @settings.yml --extra-vars @repo_settings.yml \
-       -v -u $remote_user -s $tags
+if [ ! -e repo_settings.yml ]; then
+  ansible-playbook -i local_hosts  \
+    playbooks/packstack/rdo_neutron_aio.yml \
+      --extra-vars @settings.yml  \
+        -v -u $remote_user -s $tags
+else
+  ansible-playbook -i local_hosts  \
+    playbooks/packstack/rdo_neutron_aio.yml \
+      --extra-vars @settings.yml --extra-vars @repo_settings.yml \
+         -v -u $remote_user -s $tags
+fi
+#need a zero exit everytime so teardown can be executed
+return 0
 }
 
-main "$@"
+#require a 0 exit code for clean.sh to execute
+main "$@" || true
+
