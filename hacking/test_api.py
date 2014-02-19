c9afaf8012fc02257b7b86977d7bca66af0aa28b
Wes Hayutin <weshayutin@gmail.com>
2014-02-18 20:34:03 -0500

    update to create rpm gpg file for fedora19
    Change-Id: I677576e91fb9d839d9aca565619966a4e867a2cf

---
+++ hacking/test_api.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import keystoneclient.v2_0.client as ksclient
+import os
+
+#from novaclient import client as nclient
+from novaclient.v1_1 import client as nclient
+
+def get_keystone_creds():
+    d = {}
+    d['username'] = os.environ['OS_USERNAME']
+    d['password'] = os.environ['OS_PASSWORD']
+    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['tenant_name'] = os.environ['OS_TENANT_NAME']
+    return d
+
+def get_nova_creds():
+    d = {}
+    d['username'] = os.environ['OS_USERNAME']
+    d['api_key'] = os.environ['OS_PASSWORD']
+    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['project_id'] = os.environ['OS_TENANT_NAME']
+    return d
+
+creds = get_keystone_creds()
+keystone = ksclient.Client(**creds)
+print(keystone.auth_token)
+
+nova_creds = get_nova_creds()
+print(nova_creds)
+nova = nclient.Client(**nova_creds)
+
+print(nova.servers.list())
+
+print(nova.floating_ips.list())
