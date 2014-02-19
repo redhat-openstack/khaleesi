8f33b367e5cfc106988e71b02a4bd556911b031c
Wes Hayutin <weshayutin@gmail.com>
2014-02-19 10:14:39 -0500

    packstack: add a yml file for specific job/distro settings
    Change-Id: I53efef9ddba2c29834269f0519aedb9c8f903607

--- run.sh
+++ run.sh
@@ -5,15 +5,25 @@ main() {
 source settings.sh
 
 if [ ! -e repo_settings.yml ]; then
+  echo "settings = settings.yml"
   ansible-playbook -i local_hosts  \
     playbooks/packstack/rdo_neutron_aio.yml \
       --extra-vars @settings.yml  \
         -v -u $remote_user -s $tags
-else
+elif [[ ! -e job_settings.yml && -e repo_settings.yml ]]; then
+  echo "settings = settings.yml, repo_settings.yml"
   ansible-playbook -i local_hosts  \
     playbooks/packstack/rdo_neutron_aio.yml \
       --extra-vars @settings.yml --extra-vars @repo_settings.yml \
          -v -u $remote_user -s $tags
+elif [[ -e job_settings.yml && -e repo_settings.yml ]]; then
+  echo "settings = settings.yml, repo_settings.yml, job_settings.yml"
+  ansible-playbook -i local_hosts  \
+    playbooks/packstack/rdo_neutron_aio.yml \
+      --extra-vars @settings.yml \
+        --extra-vars @repo_settings.yml \
+          --extra-vars @job_settings.yml \
+             -v -u $remote_user -s $tags
 fi
 #need a zero exit everytime so teardown can be executed
 return 0
