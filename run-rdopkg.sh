68ee4d40d6945778e6956c416aecd318e32748a0
Wes Hayutin <weshayutin@gmail.com>
2014-02-22 09:54:57 -0500

    snaphsot: ansible is not accepting yes for snapshot variable
    Had to workaround ansible bug where yes -> True
    Other snapshot updates
    
    Change-Id: If51479a8cbff1b3a934ae74288a4f7f43021e8a1

---
+++ run-rdopkg.sh
@@ -0,0 +1,50 @@
+#! /usr/bin/env bash
+set -e -u
+main() {
+
+source settings.sh
+
+if [ ! -e repo_settings.yml ]; then
+  echo "settings = settings.yml"
+  ansible-playbook -i local_hosts  \
+    playbooks/packstack/rdo_neutron_aio.yml \
+      --extra-vars @settings.yml  \
+        -v -u $remote_user -s $tags
+elif [[ ! -e job_settings.yml && -e repo_settings.yml ]]; then
+  echo "settings = settings.yml, repo_settings.yml"
+  ansible-playbook -i local_hosts  \
+    playbooks/packstack/rdo_neutron_aio.yml \
+      --extra-vars @settings.yml --extra-vars @repo_settings.yml \
+         -v -u $remote_user -s $tags
+elif [[ -e job_settings.yml && -e repo_settings.yml ]]; then
+  echo "settings = settings.yml, repo_settings.yml, job_settings.yml"
+  ansible-playbook -i local_hosts  \
+    playbooks/packstack/rdo_neutron_aio.yml \
+      --extra-vars @settings.yml \
+        --extra-vars @repo_settings.yml \
+          --extra-vars @job_settings.yml \
+             -v -u $remote_user -s $tags
+fi
+#need a zero exit everytime so teardown can be executed
+return 0
+}
+
+collect_logs() {
+  ansible-playbook -i local_hosts  \
+    playbooks/collect_logs.yml \
+      --extra-vars @settings.yml  \
+        -v -u $remote_user -s
+}
+
+snapshot() {
+  ansible-playbook -i local_hosts  \
+    playbooks/snapshot.yml \
+      --extra-vars @settings.yml  \
+        --extra-vars @job_settings.yml  \
+          -vvvv -u $remote_user -s --tags=snapshot #--step
+}
+
+
+#require a 0 exit code for clean.sh to execute
+main "$@" || true
+snapshot
