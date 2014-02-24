2f89eb4afd49c4a39a4d62aa21dcae6f46992304
Wes Hayutin <weshayutin@gmail.com>
2014-02-24 13:45:29 -0500

    update for snapshots
    Change-Id: I3da147f2ffef68613d6685804984440217afa733

--- run-rdopkg.sh
+++ run-rdopkg.sh
@@ -2,28 +2,36 @@
 set -e -u
 main() {
 
+playbook=${1:-'aio.yml'}
+echo $playbook
+
 source settings.sh
+
 
 if [ ! -e repo_settings.yml ]; then
   echo "settings = settings.yml"
   ansible-playbook -i local_hosts  \
-    playbooks/packstack/rdo_neutron_aio.yml \
+    playbooks/packstack/$playbook \
       --extra-vars @settings.yml  \
+        --extra-vars @nodes.yml  \
         -v -u $remote_user -s $tags
 elif [[ ! -e job_settings.yml && -e repo_settings.yml ]]; then
   echo "settings = settings.yml, repo_settings.yml"
   ansible-playbook -i local_hosts  \
-    playbooks/packstack/rdo_neutron_aio.yml \
-      --extra-vars @settings.yml --extra-vars @repo_settings.yml \
-         -v -u $remote_user -s $tags
+    playbooks/packstack/$playbook \
+      --extra-vars @settings.yml \
+        --extra-vars @nodes.yml  \
+          --extra-vars @repo_settings.yml \
+           -v -u $remote_user -s $tags
 elif [[ -e job_settings.yml && -e repo_settings.yml ]]; then
   echo "settings = settings.yml, repo_settings.yml, job_settings.yml"
   ansible-playbook -i local_hosts  \
-    playbooks/packstack/rdo_neutron_aio.yml \
+    playbooks/packstack/$playbook \
       --extra-vars @settings.yml \
         --extra-vars @repo_settings.yml \
           --extra-vars @job_settings.yml \
-             -v -u $remote_user -s $tags
+            --extra-vars @nodes.yml  \
+              -v -u $remote_user -s $tags
 fi
 #need a zero exit everytime so teardown can be executed
 return 0
@@ -33,18 +41,27 @@ collect_logs() {
   ansible-playbook -i local_hosts  \
     playbooks/collect_logs.yml \
       --extra-vars @settings.yml  \
-        -v -u $remote_user -s
+        --extra-vars @nodes.yml  \
+        -u $remote_user -s -vvv
 }
 
 snapshot() {
   ansible-playbook -i local_hosts  \
     playbooks/snapshot.yml \
       --extra-vars @settings.yml  \
-        --extra-vars @job_settings.yml  \
-          -vvvv -u $remote_user -s --tags=snapshot #--step
+        --extra-vars @nodes.yml  \
+          --extra-vars @job_settings.yml  \
+            -vvvv -u $remote_user -s --tags=snapshot
 }
 
+if [ ! -e nodes.yml ]; then
+  echo "Please create a nodes.yml file to define your environment"
+  echo "See https://github.com/redhat-openstack/khaleesi/blob/master/doc/packstack.md"
+  exit 1
+fi
 
 #require a 0 exit code for clean.sh to execute
 main "$@" || true
 snapshot
+#collect_logs
+
