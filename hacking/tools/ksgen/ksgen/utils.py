5d620bbc8702f39ce343db6c238caf9db66ececf
Sunil Thaha <sthaha@redhat.com>
2014-06-07 00:57:29 +1000

    ksgen/utils: to_list function
    Change-Id: Ibd77caabd7c738d9682515b9f5cd0e1246f4e547

--- hacking/tools/ksgen/ksgen/utils.py
+++ hacking/tools/ksgen/ksgen/utils.py
@@ -9,3 +9,5 @@ def extract_value(mapping, key, default=None, optional=True):
     val = val or mapping[key]    # reuse val already set
     del mapping[key]
     return val
+
+to_list = lambda x: x if isinstance(x, (list, tuple)) else [x]
