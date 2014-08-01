437e5e5efc8ac2e0e83b7c5c1923d4b78d36c0b2
weshayutin <whayutin@redhat.com>
2014-08-01 09:39:03 -0400

    update settings file for cleanup
    * make current hardcoded option the default
    * add ability to pass in file name
    
    Change-Id: Iacb26669ba0413d372282baf5bea0d6c01434b5a

--- hacking/cleanup.sh
+++ hacking/cleanup.sh
@@ -1,13 +1,14 @@
 #! /usr/bin/env bash
 set -e -u
 source functions.bash
+SETTINGS_FILE=${1:-run_settings.yml}
 
 cleanup() {
   echo "** tear down all nodes **"
   echo "** function output saved to teardown.txt **"
   local cmdline="ansible-playbook -i local_hosts  \
                 playbooks/cleanup.yml \
-                --extra-vars @run_settings.yml -v "
+                --extra-vars @$SETTINGS_FILE -v "
   execute $cmdline > teardown.txt 2>&1
 }
 
