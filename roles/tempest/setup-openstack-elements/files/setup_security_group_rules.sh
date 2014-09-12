4a9bae2a8360b13838f960090a881fe9c50ee255
wes hayutin <weshayutin@gmail.com>
2014-09-12 16:48:03 -0400

    update security group rules, open up tcp and icmp all
    * test w/ playbook validate_openstack.yml
    
    Change-Id: I8ffbe3188f0841933524d131892b2aa1b2e8f1ad

--- roles/tempest/setup-openstack-elements/files/setup_security_group_rules.sh
+++ roles/tempest/setup-openstack-elements/files/setup_security_group_rules.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 source /root/keystonerc_demo
 neutron security-group-rule-create --direction ingress --protocol icmp default
-neutron security-group-rule-create --direction ingress --protocol tcp \
-    --port_range_max 22 --port_range_min 22 default
+neutron security-group-rule-create --direction egress --protocol icmp default
+neutron security-group-rule-create --direction ingress --protocol tcp default
+neutron security-group-rule-create --direction egress --protocol tcp default
 
 exit 0
