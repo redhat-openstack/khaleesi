ea96c1f7bf86208540c2f8f6f7e923421ae4142e
wes hayutin <weshayutin@gmail.com>
2014-10-30 14:56:19 -0400

    foreman has jobs that mix distros and the group_vars working for that use
    * testing out a script vs.. reboot or shutdown which vary in their use across
    * distros and versions
    
    Change-Id: I79d5dded7ee4e020b00f85f2012d02fd4cecc482

---
+++ roles/common-handlers/files/reboot.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e
+
+function get_distribution() {
+ local release=""
+ local release_string="$(cat /etc/redhat-release)"
+ if [[ "${release_string}" == *Fedora* ]]; then release="Fedora"
+ elif [[ "${release_string}" == *Red* ]]; then release="RedHat"
+ elif [[ "${release_string}" == *CentOS* ]]; then release="CentOS"
+ else
+   echo ERROR: unable to determine distribution 1>&2
+   exit 1;
+ fi
+ echo $release
+}
+
+function get_release() {
+ local version="$(cat /etc/redhat-release | awk '{print $(NF-1) }')"
+ if [[ "${version}" == *Beta* ]]; then
+   version="$(cat /etc/redhat-release | awk '{print $(NF-2) }')"
+ elif [[ ${version%.*} -gt 0 ]]; then
+   echo OK: found version 1>&2
+ else
+   echo ERROR: unable to determine distribution, got $version 1>&2
+   exit 1;
+ fi
+ echo ${version%.*}
+}
+
+DISTRIBUTION=$(get_distribution)
+RELEASE=$(get_release)
+echo DISTRIBUTION=$DISTRIBUTION
+echo RELEASE=$RELEASE
+
+if [[ $DISTRIBUTION == "Fedora" ]]; then
+  command="/sbin/shutdown -r +1 --no-wall"
+elif [ $DISTRIBUTION == "RedHat" ] && [[ $RELEASE -lt 7 ]]; then
+  command="/sbin/shutdown -r +1 "
+elif [ $DISTRIBUTION == "CentOS" ] && [[ $RELEASE -lt 7 ]]; then
+  command="/sbin/shutdown -r +1 "
+elif [ $DISTRIBUTION == "RedHat" ] && [[ $RELEASE -ge 7 ]]; then
+  command="/sbin/shutdown -r +1 --no-wall"
+elif [ $DISTRIBUTION == "CentOS" ] && [[ $RELEASE -ge 7 ]]; then
+  command="/sbin/shutdown -r +1 --no-wall"
+else
+  exit 1
+fi
+
+echo $command
+$command
