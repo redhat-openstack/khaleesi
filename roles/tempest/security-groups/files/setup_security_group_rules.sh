9aeef3b37a2c98c4d3f10d72d7574621c386e27e
tkammer <tkammer@redhat.com>
2014-11-26 15:39:22 +0200

    Fix the tempest setup process for icehouse/juno
    This patch fixes the setup process of the nodes
    It removes the setup of openstack elements for icehouse/juno
    It fixes the default security rules that blocks initial communication
    It removes the need to download the cirros image for icehouse/juno
    as the tempest auto configuration tool does it anyway.
    
    Change-Id: Ide366d7a13465f945458d657cc51d54280668615

--- roles/tempest/security-groups/files/setup_security_group_rules.sh
+++ roles/tempest/security-groups/files/setup_security_group_rules.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-source /root/keystonerc_demo
-neutron security-group-rule-create --direction ingress --protocol icmp default
-neutron security-group-rule-create --direction egress --protocol icmp default
-neutron security-group-rule-create --direction ingress --protocol tcp default
-neutron security-group-rule-create --direction egress --protocol tcp default
+source /root/keystonerc_admin
+
+# First remove all rules from the default group
+for rule in `neutron security-group-rule-list | grep default | awk '{print $2}'`; do neutron security-group-rule-delete $rule; done
+
+# Populate the default sec-group to allow communication
+neutron security-group-rule-create --direction ingress default
+neutron security-group-rule-create --direction egress default
 
 exit 0
