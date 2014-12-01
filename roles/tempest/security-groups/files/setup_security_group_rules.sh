4c9e23955b5de7edbc2bcce571cbf652016aabe3
tkammer <tkammer@redhat.com>
2014-12-01 19:39:08 +0200

    Fixes the security group patch
    Change-Id: If87c840e2d9cd056e514c930f75addd313bd09ed

--- roles/tempest/security-groups/files/setup_security_group_rules.sh
+++ roles/tempest/security-groups/files/setup_security_group_rules.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 source /root/keystonerc_admin
 
-# First remove all rules from the default group
-for rule in `neutron security-group-rule-list | grep default | awk '{print $2}'`; do neutron security-group-rule-delete $rule; done
+# Get the admin tenant id
+admin_tenant_id=$(keystone tenant-get admin | grep id | awk '{print $4}')
+security_group_id=$(neutron security-group-list -c id -c tenant_id | grep $admin_tenant_id | awk '{print $2}')
+
+# First remove all rules from the admin default group
+for rule in `neutron security-group-rule-list -c id -c tenant_id | grep $admin_tenant_id | awk '{print $2}'`; do neutron security-group-rule-delete $rule; done
 
 # Populate the default sec-group to allow communication
-neutron security-group-rule-create --direction ingress default
-neutron security-group-rule-create --direction egress default
-
-exit 0
+for direction in ingress egress; do neutron security-group-rule-create --direction $direction $security_group_id; done
