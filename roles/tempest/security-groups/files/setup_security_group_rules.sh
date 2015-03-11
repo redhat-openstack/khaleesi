8b1a47c2071bd51febbe04bf4808b5fbbd868abb
wes hayutin <weshayutin@gmail.com>
2015-03-10 21:56:43 -0400

    fix security group setup for tempest
    Change-Id: Ib0aa0793ec7ee36551ad7e69ad623f7702e47d22

--- roles/tempest/security-groups/files/setup_security_group_rules.sh
+++ roles/tempest/security-groups/files/setup_security_group_rules.sh
@@ -2,7 +2,7 @@
 source ~/keystonerc_admin
 
 # Get the admin tenant id
-admin_tenant_id=$(keystone tenant-get admin | grep id | awk '{print $4}')
+admin_tenant_id=$(keystone tenant-get admin | grep id | head -n 1 | awk '{print $4}')
 security_group_id=$(neutron security-group-list -c id -c tenant_id | grep $admin_tenant_id | awk '{print $2}')
 
 # First remove all rules from the admin default group
