7d32cbafd3211d4e36dbd4357f829ff7119e1a75
wes hayutin <weshayutin@gmail.com>
2015-03-19 15:40:53 -0400

    updated wrapper instructions
    Change-Id: Ibe9149817020c96588aa89b17dfed120a8301aa5

--- tools/wrappers/rdo-manager-settings.sh
+++ tools/wrappers/rdo-manager-settings.sh
@@ -1,14 +1,21 @@
-#settings file to be used with component_test.sh
-#source this file then copy component_test.sh to $WORKSPACE and execute
-WORKSPACE=CHANGE_ME #base path of git checkouts
+#settings file to be used with rdo-manager-test.sh
+#source this file then copy rdo-manager-test.sh to $WORKSPACE and execute
+
+# READ_ME: Prior to executing rdo-manager-test.sh ensure ssh
+# can connect to the $TEST_BED_IP as root via ssh keys.
+# e.g. ssh-copy-id root@$TESTBED_IP
+
+# Also ensure any value marked CHANGE_ME is updated
+
+WORKSPACE=CHANGE_ME #base path of git checkouts, e.g. /tmp
 TESTBED_IP=CHANGE_ME #hostname or ip of baremetal box
-TESTBED_USER=stack  #don't use root here
+TESTBED_USER=stack  #don't use root here,only use stack
 CONFIG_BASE=$WORKSPACE/khaleesi-settings
 TEST_COMPONENT=instack-undercloud
 TEST_COMPONENT_URL=git@github.com:rdo-management/instack-undercloud.git
 PROVISION_OPTION=skip_provision  #[skip_provision, execute_provision] for baremetal box
 DISTRO=rhel-7.1
-PRIVATE_KEY=CHANGE_ME
-KHALEESI_SETTINGS=CHANGE_ME
+PRIVATE_KEY=CHANGE_ME  # ~/.ssh/id_rsa
+KHALEESI_SETTINGS=CHANGE_ME #git url to khaleesi-settings internal only atm.
 KHALEESI=https://github.com/redhat-openstack/khaleesi.git
-ANSIBLE_SETTINGS=packstack/jenkins/ansible_settings.sh
+ANSIBLE_SETTINGS=packstack/jenkins/ansible_rdo_mang_settings.sh
