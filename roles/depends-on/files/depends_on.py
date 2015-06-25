caecb0bc5377c1a71c16fd050e02824c109446b9
Attila Darazs <adarazs@redhat.com>
2015-06-25 15:54:14 +0200

    depends-on: fix the gerrithub hostname:port
    Change-Id: I2ea260dfdde11a5821b65c5b652ddb1d2b11e9bc

--- roles/depends-on/files/depends_on.py
+++ roles/depends-on/files/depends_on.py
@@ -30,7 +30,7 @@ import sys
 import urlparse
 
 # we ignore any other host reference
-ALLOWED_HOSTS = ["", "codeng", "review.gerrithub.io"]
+ALLOWED_HOSTS = ["", "codeng", "review.gerrithub.io:29418"]
 
 def parse_commit_msg(msg=None):
     """Look for dependency links in the commit message."""
