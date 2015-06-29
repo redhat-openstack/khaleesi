841b2f2f1f3adad12ee50a7c70212b469af32585
Mathieu Bultel <mbultel@redhat.com>
2015-06-29 10:57:02 +0200

    fix documentation project name in sphinx config
    Change-Id: I139815faf3d796624a46869513f5e309043514eb

--- doc/conf.py
+++ doc/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'khaleesi-settings'
-copyright = u'2014, RedHat RDO CI team'
+project = u'khaleesi'
+copyright = u'2015, Red Hat RDO CI Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
