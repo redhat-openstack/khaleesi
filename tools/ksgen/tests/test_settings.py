595cfd2036c9cdab3934f6387838ba97189aa8a4
Yair Fried <yfried@redhat.com>
2015-07-05 17:21:31 +0300

    [KSGEN] Adds tests for ksgen defaults
    Adds data files for manual testing of the ksgen defaults
    Adds unittests for automation.
    
    Change-Id: Id7bfad0ff9189ac7fc88bae9fc51b6ebae514cc3

--- tools/ksgen/tests/test_settings.py
+++ tools/ksgen/tests/test_settings.py
@@ -5,11 +5,18 @@ Usage:
 """
 
 import os
+
+import docopt
 import pytest
+from unittest import TestCase
 import yaml
 
 from test_utils import TEST_DIR, main
 from ksgen.settings import load_configuration
+from ksgen.settings import Generator
+
+
+SETTINGS_DIR = os.path.join(TEST_DIR, "data", "settings")
 
 
 def test_invalid_yml():
@@ -30,6 +37,36 @@ def test_invalid_yml():
     yaml_path = os.path.join(path, 'map.yml')
     with pytest.raises(TypeError):
         load_configuration(yaml_path)
+
+
+class TestGenerator(TestCase):
+
+    def setUp(self):
+        """Create Generator object. """
+        args = ['--provisioner=openstack', os.devnull]
+        self.obj = Generator(config_dir=SETTINGS_DIR,
+                             args=args)
+
+    def test__prepare_defaults(self):
+        provisioner_defaults = {
+            "site": "qeos",
+            "topology": "all-in-one"
+        }
+
+        site_defaults = {
+            "user": "rhos-jenkins"
+        }
+
+        self.obj.parsed = docopt.docopt(self.obj._doc_string,
+                                        options_first=True, argv=self.obj.args)
+        self.obj._prepare_defaults()
+
+        for key, val in provisioner_defaults.iteritems():
+            opt = "--provisioner-" + key
+            assert self.obj.parsed[opt] == val
+
+        assert (self.obj.parsed["--provisioner-site-user"] ==
+                site_defaults["user"])
 
 
 if __name__ == '__main__':
