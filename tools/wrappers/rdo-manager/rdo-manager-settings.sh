7b029aa0906c1f24ecdcfb0f8d085ddeae1f5e6f
Wesley Hayutin <weshayutin@gmail.com>
2015-09-04 09:59:08 -0400

    update wrapper for rdo-manager
    Change-Id: I5b868606ee5deb382882fdafcd383f308fa38cf4

--- tools/wrappers/rdo-manager/rdo-manager-settings.sh
+++ tools/wrappers/rdo-manager/rdo-manager-settings.sh
@@ -11,16 +11,22 @@
 # bash -x ./rdo-manager-test.sh
 
 
-WORKSPACE=CHANGE_ME                     # base path of git checkouts, e.g. /tmp/<dir>
+#WORKSPACE=CHANGE_ME                     # base path of git checkouts, e.g. /tmp/<dir>
+#CONFIG_BASE=$WORKSPACE/khaleesi-settings
 TESTBED_IP=CHANGE_ME                    # hostname or ip of baremetal box
-CONFIG_BASE=$WORKSPACE/khaleesi-settings
-TEST_COMPONENT=instack-undercloud
-TEST_COMPONENT_URL=https://github.com/rdo-management/instack-undercloud.git
-PROVISION_OPTION=skip_provision         # [skip_provision, execute_provision] for baremetal box
 DISTRO=centos                           # [rhel, centos]
+PRODUCT=rdo                             # [rdo, rhos]  #rhos is internal only, requires internal khaleesi-settings
+PRODUCT_VERSION=kilo                    # [kilo, 7_director] # 7_director is internal only
+PRODUCT_VERSION_REPO=delorean_mgt       # [delorean_mgt, puddle]
+PRODUCT_VERSION_BUILD=last_known_good   # [last_known_good, latest]
 DISTRO_VERSION=7.0                      # atm 7.1 for rhel, 7.0 for centos
+INSTALLER_IMAGES=build                  # [build, import] #import only used for puddles
+INSTALLER_POST_ACTION=default           # [default, none, scale_up_delete]
+INSTALLER_NETWORK_VARIANT=gre           # [gre, ml2-vxlan]
+INSTALLER_NETWORK_ISOLATION=none        # [none, single_nic_vlans]
+INSTALLER_TEMPEST=api                   # [api, disabled, minimal, scenario, smoke]
 YUM_REPO_OVERRIDE=none                  # [rhos-release,rhsm,none] none is used for centos
-PRIVATE_KEY=CHANGE_ME                   # ~/.ssh/id_rsa
+PRIVATE_KEY=~/.ssh/id_rsa                   # ~/.ssh/id_rsa
 KHALEESI_SETTINGS=https://github.com/redhat-openstack/khaleesi-settings.git #internal redhat repo required for rhel
 KHALEESI=https://github.com/redhat-openstack/khaleesi.git
-ANSIBLE_SETTINGS=jenkins/ansible_rdo_mang_settings.sh
+#ANSIBLE_SETTINGS=jenkins/ansible_rdo_mang_settings.sh
--- tools/wrappers/rdo-manager/rdo-manager-test.sh
+++ tools/wrappers/rdo-manager/rdo-manager-test.sh
@@ -1,7 +1,8 @@
-#!/bin/bash -ex
-#script to be used with component_settings.sh
+#!/bin/bash -e
+#script to be used with rdo-manager-settings.sh
 
 function ensure_khaleesi() {
+    printf '\n'; printf '*************'; printf " GET GIT REPOS "; printf '*************'; printf '\n\n'
     export GIT_SSL_NO_VERIFY=true;
     if [ ! -d khaleesi ]; then
     git clone $KHALEESI
@@ -9,128 +10,82 @@ function ensure_khaleesi() {
     if [ ! -d khaleesi-settings ]; then
     git clone $KHALEESI_SETTINGS
     fi
-    source khaleesi-settings/packstack/jenkins/ansible_settings.sh
+    #source khaleesi-settings/packstack/jenkins/ansible_settings.sh
     export CONFIG_BASE="${TOP}/khaleesi-settings"
 }
 
 function ensure_rpm_prereqs() {
+    printf '\n'; printf '**************'; printf " INSTALL REQUIRED RPMS "; printf '**************'; printf '\n\n'
     sudo yum install -y git rsync python-pip python-virtualenv gcc
 }
 
-function ensure_component() {
-    if [ ! -d $TEST_COMPONENT ]; then
-    git clone $TEST_COMPONENT_URL $TEST_COMPONENT
-    fi
-}
 
 function ensure_ansible() {
+    printf '\n'; printf '**************'; printf " INSTALL ANSIBLE "; printf '**************'; printf '\n\n'
     if [ ! -d ansible_venv ]; then
     virtualenv ansible_venv
     fi
     source ansible_venv/bin/activate
-    pip install -U ansible==1.8.2
+    pip install -U ansible==1.9.2
     pip install markupsafe
 }
 
 function ensure_ksgen() {
+    printf '\n'; printf '**************'; printf " CREATE KSGEN_SETTINGS.YML "; printf '**************'; printf '\n\n'
     if ! which ksgen >/dev/null 2>&1; then
     pushd khaleesi/tools/ksgen
     python setup.py develop
     popd
     fi
     pushd khaleesi
-    export JOB_NAME=''
-    export BEAKER_MACHINE=$TESTBED_IP
-    ksgen --config-dir=$CONFIG_BASE/settings generate \
-    --rules-file=$CONFIG_BASE/rules/instack-virt-rdo.yml \
-    --provisioner=beaker \
-    --provisioner-site=bkr \
-    --provisioner-distro=$DISTRO \
-    --provisioner-distro-version=$DISTRO_VERSION \
-    --provisioner-site-user=rdoci-jenkins \
-    --provisioner-options=skip_provision \
-    --product-version=kilo \
-    --product-version-build=last_known_good \
-    --product-version-repo=delorean_mgt \
-    --product-version-workaround=$DISTRO-$DISTRO_VERSION \
-    --workarounds=disabled \
+    export TEST_MACHINE=$TESTBED_IP
+    ksgen --config-dir=../khaleesi-settings/settings generate \
+    --provisioner=manual \
     --distro=$DISTRO-$DISTRO_VERSION \
+    --product=$PRODUCT \
+    --product-version=$PRODUCT_VERSION \
+    --product-version-build=$PRODUCT_VERSION_BUILD \
+    --product-version-repo=$PRODUCT_VERSION_REPO \
+    --installer=rdo_manager \
+    --installer-deploy=templates \
+    --installer-env=virthost \
+    --installer-images=$INSTALLER_IMAGES \
     --installer-network=neutron \
-    --installer-network-variant=gre \
-    --installer-messaging=rabbitmq \
-    --tester=tempest \
-    --tester-setup=rpm \
-    --tester-tests=minimal \
-    --extra-vars product.repo_type_override=$YUM_REPO_OVERRIDE \
+    --installer-network-isolation=$INSTALLER_NETWORK_ISOLATION \
+    --installer-network-variant=$INSTALLER_NETWORK_VARIANT \
+    --installer-post_action=$INSTALLER_POST_ACTION \
+    --installer-topology=minimal \
+    --installer-tempest=$INSTALLER_TEMPEST \
+    --workarounds=enabled \
+    --extra-vars product.repo_type_override=none \
+    --extra-vars @../khaleesi-settings/hardware_environments/virt/network_configs/$INSTALLER_NETWORK_ISOLATION/hw_settings.yml \
     ksgen_settings.yml
     popd
 }
 
 function ensure_ansible_connection(){
-    pushd khaleesi
-    cat <<EOF >instack_hosts
-[instack-virt-host]
-$TESTBED_IP groups=testbed ansible_ssh_host=$TESTBED_IP ansible_ssh_user=root
-
-[local]
-localhost ansible_connection=local
-EOF
-
-    ansible -i instack_hosts  \
-        -u root \
-        --private-key=$PRIVATE_KEY \
-        -vvvv -m ping all
-    connection=$?
-    popd
-    echo $connection
+    printf '\n'; printf '**************'; printf " TEST CONNECTION TO $TEST_MACHINE "; printf '**************'; printf '\n\n'
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$TEST_MACHINE ls -la
+    return $?
 }
 
 function ensure_ssh_key() {
-    if ! ensure_ansible_connection; then
-        ssh-copy-id "root@${TESTBED_IP}"
-    else
-        echo "ssh keys are properly set up"
-    fi
+    printf '\n'; printf '**************'; printf " ENSURE SSH KEYS ARE ON $TEST_MACHINE "; printf '**************'; printf '\n\n'
+    ssh-copy-id -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "root@${TESTBED_IP}"
 }
 
-function configure_ansible_hosts() {
-    pushd khaleesi
-    rm -Rf instack_hosts
-    cat <<EOF >instack_hosts
-[instack-virt-host]
-$TESTBED_IP groups=testbed ansible_ssh_host=$TESTBED_IP ansible_ssh_user=stack
-
-[local]
-localhost ansible_connection=local
-EOF
-    popd
-}
 
 function configure_ansible_cfg() {
+    printf '\n'; printf '**************'; printf " CREATE ANSIBLE.CFG "; printf '**************'; printf '\n\n'
     pushd khaleesi
     if [ ! -f  ansible.cfg ]; then
-    cat <<EOF >ansible.cfg
-[defaults]
-host_key_checking = False
-roles_path = ./roles
-library = ./library:$VIRTUAL_ENV/share/ansible/
-ssh_args =  -F ssh.config.ansible
-pipelining=True
-callback_plugins = plugins/callbacks/
-EOF
+    cp ansible.cfg.example ansible.cfg
+    echo "ssh_args = -F ssh.config.ansible" >> ansible.cfg
+    touch ssh.config.ansible
     fi
     popd
 }
 
-function run_ansible_cleanup() {
-    pushd khaleesi
-    ansible-playbook -vv \
-    --private-key=${PRIVATE_KEY} \
-    -i instack_hosts \
-    --extra-vars @ksgen_settings.yml \
-    playbooks/cleanup_libvirt.yml
-    popd
-}
 
 function test_git_checkout() {
     if [ ! -d khaleesi-settings ]; then
@@ -145,12 +100,12 @@ function test_git_checkout() {
 }
 
 function run_ansible_rdo_manager() {
+    printf '\n'; printf '**************'; printf " CREATE ANSIBLE.CFG "; printf '**************'; printf '\n\n'
     pushd khaleesi
     ansible-playbook -vv \
-    --private-key=${PRIVATE_KEY} \
-    -i instack_hosts \
+    -i local_hosts \
     --extra-vars @ksgen_settings.yml \
-    playbooks/rdo-manager.yml
+    playbooks/full-job-no-test.yml
     popd
 }
 
@@ -162,16 +117,12 @@ fi
 TOP=$(pwd)
 source rdo-manager-settings.sh
 ensure_rpm_prereqs
-ensure_component
 ensure_khaleesi
 test_git_checkout
-ensure_component
 ensure_ansible
 ensure_ksgen
+ensure_ssh_key
 ensure_ansible_connection
 configure_ansible_cfg
-ensure_ssh_key
-configure_ansible_hosts
-run_ansible_cleanup
 run_ansible_rdo_manager
 
