8583865a201682dc862b6680558351100b30693e
John Trowbridge <trown@redhat.com>
2015-12-09 09:38:26 -0500

    [jjb] Fix centos delorean promote
    Change-Id: I859a89e5620726d63fe95311cf8eeacce97ad4b3

--- jenkins-jobs/scripts/centos-liberty.sh
+++ jenkins-jobs/scripts/centos-liberty.sh
@@ -2,3 +2,4 @@ set -e
 export RDO_VERSION='centos-liberty';
 export DELOREAN_URL='http://trunk.rdoproject.org/centos7-liberty/current/delorean.repo';
 export RDO_VERSION_DIR='liberty';
+export DELOREAN_HOST='trunk.rdoproject.org'
--- jenkins-jobs/scripts/promote-execute-promote.sh
+++ jenkins-jobs/scripts/promote-execute-promote.sh
@@ -1,2 +1,3 @@
-echo $delorean_current_hash
-#ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/rhos-ci -p 330 promoter@$DELOREAN_HOST "sudo /usr/local/bin/promote.sh $HASH" $RDO_VERSION || true
+export PROMOTE_HASH=`echo $delorean_current_hash | awk -F '/' '{ print $3}'`
+
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/rhos-ci -p 3300 promoter@$DELOREAN_HOST "sudo /usr/local/bin/promote.sh $PROMOTE_HASH" $RDO_VERSION || true
