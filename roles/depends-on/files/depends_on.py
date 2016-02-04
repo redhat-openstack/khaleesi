31c1554237bfe53de223fd85266c8bdd9d7baf6c
Adriano Petrich <apetrich@redhat.com>
2016-02-04 13:33:18 +0000

    Make depends-on generate the rpm building settings
    Change the way depends-on work
    
    if we know how to build packages for the dependence
    it will generate an extra_settings_<num>.yml for each package
    
    that can be called with
    
    ansible-playbook -i hosts -e @ksgen_settings.yml -e @extra_settings_1.yml playbooks/build_gate_rpm.yml
    
    to build the extra packages
    
    if we don't have the settings/rpms/<package>.yml
    but we have a directory there it will fetch the patch for that directory
    
    ( this is the case for depends-on changes to khaleesi and
    khaleesi-settings )
    
    these are not real dependencies it is just for gating
    depends-on:I911f6c47f@codeng
    depends-on:I6503cb7e6@codeng
    
    Change-Id: I6bfa13de7bbf989ddaf2a982f4300c7e74c989d7

--- roles/depends-on/files/depends_on.py
+++ roles/depends-on/files/depends_on.py
@@ -28,9 +28,14 @@ import shlex
 import subprocess
 import sys
 import urlparse
+import yaml
+from argparse import ArgumentParser
+from glob import glob
+from jinja2 import Template
 
 # we ignore any other host reference
 ALLOWED_HOSTS = ["", "codeng", "review.gerrithub.io:29418"]
+
 
 def parse_commit_msg(msg=None):
     """Look for dependency links in the commit message."""
@@ -59,6 +64,7 @@ def parse_commit_msg(msg=None):
                       tag.group(1), tag.group(2))
     return tags
 
+
 def get_refspec_urls(tags):
     """Parsing the necessary url info for the referenced changes"""
     def_host = os.getenv("GERRIT_HOST", 'review.gerrithub.io')
@@ -79,10 +85,16 @@ def get_refspec_urls(tags):
         output = subprocess.check_output(shlex.split(cmd)).splitlines()[0]
         # parse it to json
         data = json.loads(output)
+
         if "currentPatchSet" not in data:
             logging.warning("failed to fetch data from gerrit for "
                             "Change-Id: %s", change)
             continue
+        if data.get("status") not in ["NEW"]:
+            logging.warning("Patch already merged "
+                            "Change-Id: %s", change)
+            continue
+
         parsed_url = urlparse.urlparse(data["url"])
         # gerrit does not provide the repo URL in the reply, we have to
         # construct it from the clues
@@ -94,42 +106,144 @@ def get_refspec_urls(tags):
         # get the repo name from the last part after the slash
         repo_folder = data["project"].split("/")[-1]
         repo_ref = data["currentPatchSet"]["ref"]
-        targets.append([repo_folder, repo_url, repo_ref])
+        repo_branch = data["branch"]
+
+        targets.append([repo_folder, repo_url, repo_ref, repo_branch])
         logging.debug("data query result for Change-Id: %s, server: %s:%s, "
-                      "folder %s, url: %s, ref: %s",
-                      change, host, port, repo_folder, repo_url, repo_ref)
+                      "folder %s, url: %s, ref: %s, branch:%s",
+                      change, host, port, repo_folder,
+                      repo_url, repo_ref, repo_branch)
     return targets
 
-def checkout_changes(targets, basedir="."):
-    """Fetch and checkout the changes for the target repos"""
+
+def update_repo(project, url, ref, branch, basedir):
     checkout_cmd = "git checkout FETCH_HEAD"
-    for folder, url, ref in targets:
-        folder_path = os.path.join(basedir, folder)
+    try:
+        # I didn't find the settings/rpm for the project
+        # so I'm going to try to fetch the changes if the
+        # directory for that project exists in the tree
+        folder_path = os.path.join(basedir, project)
         logging.debug("changing working dir to %s", folder_path)
         os.chdir(folder_path)
         fetch_cmd = "git fetch %s %s" % (url, ref)
         logging.debug("fetch command: %s", fetch_cmd)
         subprocess.Popen(shlex.split(fetch_cmd)).wait()
         subprocess.Popen(shlex.split(checkout_cmd)).wait()
+    except OSError:
+        logging.warning(
+            "Directory not found for {} skipping".format(project)
+        )
 
-def test_module():
+
+def update_rpm(project, ref, branch, basedir, ksgen, filenumber):
+    output_dict = {}
+    # doing a late evaluation on ksgen_settings existence
+    # because it might not be needed
+    if not ksgen:
+        logging.error(
+            "ksgen_settings not found"
+        )
+        sys.exit(1)
+
+    rpm_instructions = glob("{}/khaleesi/settings/rpm/*{}.yml".format(basedir, project))
+    if not rpm_instructions:
+        logging.warning(
+            "khaleesi/settings/rpm/*{}.yml not found in {}".format(project, basedir)
+        )
+        return
+
+    with open(rpm_instructions[0], "r") as fd:
+        # the replace here is important because !lookup is not
+        # valid jinja2 template and it will be used later
+        output_dict = yaml.load(fd.read().replace("!lookup", ""))
+
+    # do the changes needed for this patch
+    output_dict["patch"]["gerrit"]["branch"] = branch
+    output_dict["patch"]["gerrit"]["refspec"] = ref
+
+    # but the change still leaves two private urls
+    # like private.building.gerrit.url
+    # luckly those exist in ksgen_settings and using
+    # jinja2 templates here will fill those values
+    t = Template(yaml.safe_dump(output_dict, default_flow_style=False))
+
+    with open("{}/khaleesi/extra_settings_{}.yml".format(basedir, filenumber), "w") as fd:
+        fd.write(t.render(ksgen))
+        fd.write("\n")  # for extra niceness
+    logging.warning(
+        "wrote {}/khaleesi/extra_settings_{}.yml for {}".format(basedir, filenumber, project)
+    )
+
+
+def generate_config(targets, basedir=".", update=None, ksgenfile=None):
+    """
+    This works in two ways
+
+    if we know how to build the package (ie. it exists on settings/rpms/)
+    we generate one extra_settings_<num>.yml for each of the packages
+
+    if we do not know how to build it but there's a directory with that name
+    under basedir we will update that to the ref specified and check that out
+
+    """
+    if not ksgenfile:
+        ksgenfile = "{}/khaleesi/ksgen_settings.yml".format(basedir)
+
+    try:
+        with open(ksgenfile, "r") as fd:
+            ksgen = yaml.load(fd)
+    except IOError:
+        ksgen = None
+
+    filenumber = 1
+    for project, url, ref, branch in targets:
+        if update == "repo":
+            update_repo(project, url, ref, branch, basedir)
+
+        elif update == "rpm":
+            update_rpm(project, ref, branch, basedir, ksgen, filenumber)
+            filenumber += 1
+
+
+def test_module(basedir, update, ksgenfile):
     """Test with some known working Change-Ids"""
     test_msg = ("This is a test commit message.\n\n"
                  "Depends-On: If4cea049\n"
-                 "Depends-On: I1c3f14ba@codeng")
+                 "Depends-On: Id0aef5ee6dcb@review.gerrithub.io:29418\n"
+                 "Depends-On: I1c3f14ba@codeng\n"
+                 "Depends-On: I62e3c43afd@codeng\n"
+                 "Depends-On: I02c15311@codeng")
     test_tags = parse_commit_msg(base64.b64encode(test_msg))
     test_targets = get_refspec_urls(test_tags)
-    checkout_changes(test_targets, "/tmp")
+    generate_config(test_targets, basedir, update, ksgenfile)
 
-def run(repo_dir):
+
+def run(basedir, update, ksgenfile):
+    logging.warning(
+        "getting dependencies for {}".format(update)
+    )
     run_tags = parse_commit_msg()
     run_targets = get_refspec_urls(run_tags)
-    checkout_changes(run_targets, repo_dir)
+    if run_targets:
+        generate_config(run_targets, basedir, update, ksgenfile)
+    else:
+        logging.warning("Nothing to do. Exiting")
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    if len(sys.argv) < 2:
-        print "Usage: %s <repo-directory>" % (sys.argv[0])
-    else:
-        run(sys.argv[1])
-        #test_module()
+    ap = ArgumentParser("Generate changes to repos or rpm settings based on depends-on gerrit comments")
+    ap.add_argument('basedir',
+                    default=".",
+                    help="basedir to work from")
+    ap.add_argument('build',
+                    default="repo",
+                    choices=['repo', 'rpm'],
+                    nargs='?',
+                    help="What to build")
+    ap.add_argument('ksgen_settings',
+                    nargs='?',
+                    help="where to find the ksgen_settings.yml file")
+    args = ap.parse_args()
+    run(args.basedir, args.build, args.ksgen_settings)
+    # test_module(args.basedir, args.build, args.ksgen_settings)
